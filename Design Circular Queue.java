19652f32208fcbde6ef7090b911bbcefbc6040b1
Prasham0105 <12202040601045@adit.ac.in>
2024-08-07 10:51:02 +0530

    Create Design Circular Queue.java

---
+++ Design Circular Queue.java
@@ -0,0 +1,73 @@
+class MyCircularQueue {
+    private int[] data;
+    private int head;
+    private int tail;
+    private int size;
+
+    public MyCircularQueue(int k) {
+        data = new int[k];
+        head = -1;
+        tail = -1;
+        size = k;
+    }
+
+    public boolean enQueue(int value) {
+        if (isFull()) {
+            return false;
+        }
+        if (isEmpty()) {
+            head = 0;
+        }
+        tail = (tail + 1) % size;
+        data[tail] = value;
+        return true;
+    }
+
+    public boolean deQueue() {
+        if (isEmpty()) {
+            return false;
+        }
+        if (head == tail) {
+            head = -1;
+            tail = -1;
+            return true;
+        }
+        head = (head + 1) % size;
+        return true;
+    }
+
+    public int Front() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return data[head];
+    }
+
+    public int Rear() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return data[tail];
+    }
+
+    public boolean isEmpty() {
+        return head == -1;
+    }
+
+    public boolean isFull() {
+        return ((tail + 1) % size) == head;
+    }
+
+    public static void main(String[] args) {
+        MyCircularQueue circularQueue = new MyCircularQueue(3); // set the size to be 3
+        System.out.println(circularQueue.enQueue(1)); // return true
+        System.out.println(circularQueue.enQueue(2)); // return true
+        System.out.println(circularQueue.enQueue(3)); // return true
+        System.out.println(circularQueue.enQueue(4)); // return false, the queue is full
+        System.out.println(circularQueue.Rear()); // return 3
+        System.out.println(circularQueue.isFull()); // return true
+        System.out.println(circularQueue.deQueue()); // return true
+        System.out.println(circularQueue.enQueue(4)); // return true
+        System.out.println(circularQueue.Rear()); // return 4
+    }
+}
