cfeeac2bd86e328e46e0a00b1c716b0f68036241
Prasham0105 <12202040601045@adit.ac.in>
2024-10-15 12:59:38 +0530

    Create LongestCommon Subsequence.java

---
+++ Practical13/LongestCommon Subsequence.java
@@ -0,0 +1,28 @@
+public class Main {
+ // Function to find the length of the longest common subsequence
+ public static int lcs(String X, String Y) {
+ int m = X.length();
+ int n = Y.length();
+ int[][] L = new int[m + 1][n + 1];
+ // Building the L[m+1][n+1] matrix in bottom-up way
+ for (int i = 0; i <= m; i++) {
+ for (int j = 0; j <= n; j++) {
+ if (i == 0 || j == 0) {
+ L[i][j] = 0;
+ } else if (X.charAt(i - 1) == Y.charAt(j - 1)) {
+ L[i][j] = L[i - 1][j - 1] + 1;
+ } else {
+ L[i][j] = Math.max(L[i - 1][j], L[i][j - 1]);
+ }
+ }
+ }
+ 
+ return L[m][n];
+ }
+ public static void main(String[] args) {
+ String X = "AGGTAB";
+ String Y = "GXTXAYB";
+ 
+ System.out.println("Length of LCS is " + lcs(X, Y));
+ }
+}
